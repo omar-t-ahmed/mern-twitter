86d7ec25e77324c1cbf8216dd4a2c553beb3fbe5
omar-t-ahmed <omartahmed@gmail.com>
2023-11-25 21:18:14 -0500

    add login

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
 
 const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
+
+require('./models/User');
+require('./config/passport');
+const passport = require('passport');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
@@ -12,6 +17,7 @@ const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
+app.use(passport.initialize());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,27 +25,47 @@ app.use(cookieParser());
 
 // Security Middleware
 if (!isProduction) {
-  // Enable CORS only in development because React will be on the React
-  // development server (http://localhost:3000). (In production, React files
-  // will be served statically on the Express server.)
-  app.use(cors());
+// Enable CORS only in development because React will be on the React
+// development server (http://localhost:3000). (In production, React files
+// will be served statically on the Express server.)
+app.use(cors());
 }
 
 // Set the _csrf token and create req.csrfToken method to generate a hashed
 // CSRF token
 app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
-  })
+    csurf({
+        cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+        }
+    })
 );
 
 // Attach Express routers
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+    })
+});
 
 module.exports = app;
