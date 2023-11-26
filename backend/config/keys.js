3986bc362537ad609ce594aa9c915a5a079e5fa7
omar-t-ahmed <omartahmed@gmail.com>
2023-11-25 21:45:08 -0500

    finish jwt setup

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,8 +1,5 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI
-}
-
-module.exports = {
+    secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
     isProduction: process.env.NODE_ENV === 'production'
-}
+};
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,14 +15,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/register', async (req, res, next) => {
-  // Check to make sure no one has already registered with the proposed email or
-  // username.
+
   const user = await User.findOne({
     $or: [{ email: req.body.email }, { username: req.body.username }]
   });
 
   if (user) {
-    // Throw a 400 error if the email address and/or email already exists
+
     const err = new Error("Validation Error");
     err.statusCode = 400;
     const errors = {};
@@ -34,7 +35,6 @@ router.post('/register', async (req, res, next) => {
     return next(err);
   }
 
-  // Otherwise create a new user
   const newUser = new User({
     username: req.body.username,
     email: req.body.email
@@ -47,14 +47,16 @@ router.post('/register', async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       }
       catch(err) {
         next(err);
       }
     })
   });
+
 });
+
 
 router.post('/login', async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
@@ -65,8 +67,22 @@ router.post('/login', async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
+});
+
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
